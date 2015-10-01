0cfa6171e72a047ee37fbba88596bc7afb92a796
Brad Sawicki <git@bradsawicki.com>
2015-10-01 18:09:53 +0200

    404 error page / btn styles

--- wp-content/themes/baker/404.php
+++ wp-content/themes/baker/404.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
 
   <section class="main">
-    <div class="wrapper padded">
-    	<h2>Whoops!</h1>
+    <div class="wrapper">
+    	<h1>Whoops!</h1>
       <div class="body">
       	<p>Looks like this page doesn't exist. Head home and try again.</p>
       	<p><a href="/" class="btn">Back Home</a></p>
